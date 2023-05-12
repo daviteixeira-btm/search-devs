e124df515b5bbc2522f0532dc85a36f5fcf32442
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-05-12 18:17:41 -0300

    Editing the Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,23 @@
 import { useState } from 'react';
-
-import { UserProps } from '../types/user';
-
-import Search from '../components/Search';
-
-import Error  from '../components/Error';
-
+import { useRecoilState } from 'recoil';
 import { useNavigate } from 'react-router-dom';
 
-import { useRecoilState } from 'recoil';
+import Error from '../components/Error';
+import Search from '../components/Search';
 
+import { UserProps } from '../types/user';
 import { userState } from '../core/atoms/user.atom';
+
 
 const Home = () => {
 
-    const [user, setUser] = useRecoilState<UserProps | null>(userState);
+    const [, setUser] = useRecoilState<UserProps | null>(userState);
 
-    const [ error, setError ] = useState(false);
+    const [error, setError] = useState(false);
 
     const navigate = useNavigate();
 
-    const loadUser = async(userName: string) => {
+    const loadUser = async (userName: string) => {
 
         setError(false);
         setUser(null);
@@ -28,36 +25,36 @@ const Home = () => {
         const response = await fetch(`https://api.github.com/users/${userName}`);
         const data = await response.json();
 
-        if(response.status === 404) {
+        if (response.status === 404) {
             setError(true);
             return;
         }
 
-        const { 
-            avatar_url,
-            login, 
-            location, 
-            followers, 
-            following, 
-            name, 
+        const {
             bio,
-            company,
-            email,
+            name,
             blog,
+            email,
+            login,
+            company,
+            location,
+            followers,
+            following,
+            avatar_url,
             twitter_username,
         } = data;
 
         const userData: UserProps = {
-            avatar_url,
+            bio,
+            name,
+            blog,
             login,
+            email,
+            company,
             location,
             followers,
             following,
-            name,
-            bio,
-            company,
-            email,
-            blog,
+            avatar_url,
             twitter_username,
         };
 
@@ -67,30 +64,38 @@ const Home = () => {
 
     return (
         <main
-            style={{ 
-                height: '100vh', 
-                display: 'flex', 
+            style={{
+                height: '100vh',
+                display: 'flex',
                 alignItems: 'center', 
-                // border: '1px solid red', 
-                flexDirection: 'column', 
+                flexDirection: 'column',
                 justifyContent: 'center',
             }}
         >
 
-            <h1 style={{ fontSize: '5rem', fontWeight: '700', color: '#8C19D2', marginBottom: '3.5rem'}}>
+            <h1
+                style={{ 
+                    fontSize: '5rem', 
+                    fontWeight: '700', 
+                    color: '#8C19D2', 
+                    marginBottom: '3.5rem' 
+                }}
+            >
                 <span style={{ color: '#0069CA' }}>Search</span> d_evs
             </h1>
-            
+
             <div
-                style={{ 
-                    width: '100%', 
-                    display: 'flex', 
-                    justifyContent: 'center'
+                style={{
+                    width: '100%',
+                    display: 'flex',
+                    marginBottom: '2rem',
+                    justifyContent: 'center',
                 }}
             >
                 <Search loadUser={loadUser} />
-                { error && <Error /> }
             </div>
+
+            {error && <Error />}
         </main>
     );
 };

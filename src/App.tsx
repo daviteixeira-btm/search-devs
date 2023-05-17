bccf6f253935633e3baa0f62a5f199c60dd9fbf6
Davi Teixeira <daviteixeira.btm@gmail.com>
2023-05-17 00:39:36 -0300

    removing useRecoil from project

--- src/App.tsx
+++ src/App.tsx
@@ -8,27 +8,18 @@ import { Suspense } from 'react';
 
 import './App.css';
 
-// Pages
-// import Home from './pages/Home';
 import HomePage from './pages/HomePage';
-
-// import Perfil from './pages/Perfil';
 import UserPerfilPage from './pages/UserPerfilPage';
 
 import 'primeicons/primeicons.css';
 import 'primereact/resources/primereact.min.css';
-import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/themes/saga-purple/theme.css';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          {/* Páginas com Recoil */}
-          {/* <Route path='/' element={<Home />} /> */}
-          {/* <Route path='/perfil' element={<Perfil />} /> */}
-
-          {/* Páginas apenas usando o React */}
           <Route path='/' element={<HomePage />} />
           <Route path='perfil/:username' element={<UserPerfilPage />} />
         </Routes>
